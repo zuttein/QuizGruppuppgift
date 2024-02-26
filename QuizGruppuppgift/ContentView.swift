25f5a17f1ff5dc22879b90148264324b674485a9
zuttein <nilssondennis97@gmail.com>
2024-02-26 14:47:06 +0100

    Initial Commit

---
+++ QuizGruppuppgift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuizGruppuppgift
+//
+//  Created by Dennis.Nilsson on 2024-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuizGruppuppgift/QuizGruppuppgiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGruppuppgiftApp.swift
+//  QuizGruppuppgift
+//
+//  Created by Dennis.Nilsson on 2024-02-26.
+//
+
+import SwiftUI
+
+@main
+struct QuizGruppuppgiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
