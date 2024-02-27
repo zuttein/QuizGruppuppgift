21bd481077cd2ec0ec6bb4a736cecbd6c6c2554e
Jocke <joakim198904@live.se>
2024-02-27 10:15:21 +0100

    PlayerSetupView basic design.

---
+++ QuizGruppuppgift/PlayerSetupView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerSetupView.swift
+//  QuizGruppuppgift
+//
+//  Created by Joakim.Bjärkstedt on 2024-02-26.
+//
+
+import SwiftUI
+
+struct PlayerSetupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerSetupView()
+}
